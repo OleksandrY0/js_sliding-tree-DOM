2ad4df30562a97216f2804f896e50f12f9f7b884
Oleksandr <sasha16092007@gmail.com>
2025-07-31 12:45:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const [...titles] = document.querySelectorAll('li');
+
+titles.forEach((title) => {
+  const span = document.createElement('span');
+
+  if (title.firstChild && title.childNodes.length > 1) {
+    span.appendChild(title.firstChild);
+    title.insertBefore(span, title.firstChild);
+  }
+});
+
+const [...spans] = document.querySelectorAll('span');
+
+spans.forEach((span) => {
+  span.addEventListener('click', (e) => {
+    if (e.target !== span) {
+      return;
+    }
+
+    const ul = span.parentElement.querySelector('ul');
+
+    if (ul) {
+      ul.classList.toggle('collapsed');
+    }
+  });
+});
